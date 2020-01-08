456ece71e98ef8f83aa2e790a50b9238bd26edc2
dr-matt-smith <matt.smith@itb.ie>
2020-01-08 09:43:12 +0000

    added files to project

---
+++ Assets/_Scripts/CheckPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private Vector3 respawnPosition;
+
+    void Start()
+    {
+        respawnPosition = transform.position;
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        print("I hit something tagged: " + hit.tag);
+        
+        if (hit.CompareTag("Checkpoint"))
+            respawnPosition = transform.position;
+
+        if (hit.CompareTag("Death"))
+        {
+            transform.position = respawnPosition;
+        }
+    }
+}
